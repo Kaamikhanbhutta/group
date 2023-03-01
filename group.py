416d4107e8436fadb402d925a6e0aca38e0c6d67
KAAMI KHAN <125378205+Kaamikhanbhutta@users.noreply.github.com>
2023-03-01 14:12:30 +0500

    Create group.py

---
+++ group.py
@@ -0,0 +1,14 @@
+import os
+print('give star to our repo')
+os.system('git pull && clear')
+os.system('xdg-open https://github.com/kaamikhanbhutta/group.py')
+print('update finish')
+print('Join Our Facebook Group and support us')
+os.system('xdg-open https://facebook.com/groups/2545075955580675/')
+
+
+os.system('clear')
+
+#print('tool is closed')
+#exit()
+import XXV
